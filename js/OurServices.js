e8ea4be67b5630d8632d8089bade88305a34b174
TymurDatsenko <tdacenko746@gmail.com>
2023-06-22 13:18:33 +0200

    updated images + deleted some files

--- js/OurServices.js
+++ js/OurServices.js
@@ -1,14 +1,14 @@
 function filterBlocks() {
-    const searchInput = document.querySelector('.input');
-    const filter = searchInput.value.toLowerCase();
-    const containers = document.querySelectorAll('.container');
+  const searchInput = document.querySelector('.input');
+  const filter = searchInput.value.toLowerCase();
+  const containers = document.querySelectorAll('.container');
 
-    containers.forEach(container => {
-        const filterValue = container.getAttribute('data-filter');
-        if (filterValue.toLowerCase().includes(filter)) {
-          container.style.display = "block";
-        } else {
-          container.style.display = "none";
-        }
-    });
+  containers.forEach(container => {
+    const filterValue = container.getAttribute('data-filter');
+    if (filterValue.toLowerCase().includes(filter)) {
+      container.style.display = "block";
+    } else {
+      container.style.display = "none";
+    }
+  });
 }
--- js/asd.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.about .video-container .controls .control-btn').forEach(btn => {
-    btn.onclick = () => {
-        let src = btn.getAttribute('data-src');
-        document.querySelector('.about .video-container .video').src = src;
-    }
-});
